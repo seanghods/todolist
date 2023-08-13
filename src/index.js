43cffaa109891061dfc4b1c057956e166cc1c270
Sean Ghods <137848705+seanghods@users.noreply.github.com>
2023-08-12 18:29:32 -0700

    Add create modal and CSS styling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css';
+
+
+
+const createButton = document.querySelector(".create");
+createButton.addEventListener('click', toggleCreate);
+
+function toggleCreate() {
+    const modal = document.querySelector(".create-modal")
+    modal.classList.toggle("active");
+}
